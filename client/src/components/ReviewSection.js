99163773a4d1aa6cf46ff43742d186dfefa55db3
Marco Kunz <kunz.marco@gmail.com>
2018-11-10 13:24:30 +0100

    refactor (client): fix borken visual load state caused by multiple autofocus

--- client/src/components/ReviewSection.js
+++ client/src/components/ReviewSection.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import SwipeableView from 'react-swipeable-views';
 import DoableForm from './DoableForm';
 
-export class ReviewSection extends Component {
+export function ReviewSection (props) {
 
   // Reviewabls are Doables that need reviewing
-  getReviewables = doables => {
-    return doables.map( (doable, i) => {
-      console.log('doable: ', doable);
-      return <DoableForm key={doable.id} data={doable} />;
-    })
+  const renderReviewables = doables => {
+    return doables.map( (doable, i) =>
+      <DoableForm key={doable.id} data={doable} /> );
   }
 
-  render() {
-    console.log('ReviewSection Render');
-
-    return (
-      <SwipeableView enableMouseEvents>
-        { this.getReviewables(this.props.doables) }
-      </SwipeableView>
-    )
-  }
+  return (
+    <SwipeableView enableMouseEvents>
+      { renderReviewables(props.doables) }
+    </SwipeableView>
+  )
 }
 
 const mapStateToProps = (state) => ({
