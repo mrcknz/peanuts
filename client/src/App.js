3d0f7a7452ad51fba1553e8af996726fe05ced51
Marco Kunz <kunz.marco@gmail.com>
2018-11-10 10:47:25 +0100

    make swipe nav work with placeholder content

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,51 @@ import React, { Component } from 'react';
 import SwipeableView from 'react-swipeable-views';
 import DoableForm from './components/DoableForm';
 import ReviewSection from './components/ReviewSection';
-import './App.css';
 
-class App extends Component {
+function App () {
 
-  state = {
-    verticalSwipeProps: {}
+  const styles = {
+    ySwipeContainer: {
+      containerStyle: {
+        height: '50vh'
+      }
+    },
+    xSwipeContainer: {
+      containerStyle: {
+        height: '50vh'
+      }
+    },
+    page: {
+      style: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%'
+      }
+    }
   }
 
-  componentDidMount () {
-    this.setState(state => ({...state, verticalSwipeProps: { index: 0 } }));
-  }
+  const renderPage = (row, num) => Array(num).fill(null).map( (page, i) => {
+    return <div key={++i} {...styles.page}>{`Page ${row}-${i}`}</div>;
+  });
 
-  render() {
-    return (
-      <SwipeableView axis="y" enableMouseEvents {...this.state.verticalSwipeProps} containerStyle={{height: '100vh'}}>
-        <DoableForm type="quickEntry" />
-        <ReviewSection />
+  return (
+    <SwipeableView axis="y" containerStyle={{height: '100vh'}}>
+      <SwipeableView containerStyle={{height: '100vh'}}>
+        {renderPage(1,1)}
       </SwipeableView>
-    )
-  }
+      <SwipeableView containerStyle={{height: '100vh'}}>
+        {renderPage(2,5)}
+      </SwipeableView>
+      <SwipeableView containerStyle={{height: '100vh'}}>
+        {renderPage(3,5)}
+      </SwipeableView>
+    </SwipeableView>
+    // <SwipeableView axis="y" enableMouseEvents {...this.state.verticalSwipeProps} containerStyle={{height: '100vh'}}>
+    //   <DoableForm type="quickEntry" />
+    //   <ReviewSection />
+    // </SwipeableView>
+  )
 }
 
 export default App;
