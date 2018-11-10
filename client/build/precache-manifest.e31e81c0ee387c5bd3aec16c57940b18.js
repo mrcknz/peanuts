fdde312ef310ab47de714417f048e89506997282
Marco Kunz <kunz.marco@gmail.com>
2018-11-10 23:22:02 +0100

    add build folder

---
+++ client/build/precache-manifest.e31e81c0ee387c5bd3aec16c57940b18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b1808fe20206aa8a62a",
+    "url": "/peanuts/static/js/runtime~main.1b1808fe.js"
+  },
+  {
+    "revision": "dec81394eeaed9341678",
+    "url": "/peanuts/static/js/main.dec81394.chunk.js"
+  },
+  {
+    "revision": "f4b2e903fae7b0c771f2",
+    "url": "/peanuts/static/js/1.f4b2e903.chunk.js"
+  },
+  {
+    "revision": "dec81394eeaed9341678",
+    "url": "/peanuts/static/css/main.78728344.chunk.css"
+  },
+  {
+    "revision": "8df7f49346a827f09a4638725463b011",
+    "url": "/peanuts/index.html"
+  }
+];
