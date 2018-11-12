b32e96ea35f9d169da60cd8490a54dd5f2c89fe4
Marco Kunz <kunz.marco@gmail.com>
2018-11-12 17:30:42 +0100

    Updates

---
+++ precache-manifest.40d59e6f31c1ead73b46e3ac4f59c8d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c405c13ebacaadfc5def2aa6f9ae7b7d",
+    "url": "/peanuts/static/media/icons_mobiscroll.c405c13e.ttf"
+  },
+  {
+    "revision": "965a7a1a7bd29d1565b5edeca7b4b9b7",
+    "url": "/peanuts/static/media/icons_mobiscroll.965a7a1a.woff"
+  },
+  {
+    "revision": "1b1808fe20206aa8a62a",
+    "url": "/peanuts/static/js/runtime~main.1b1808fe.js"
+  },
+  {
+    "revision": "e3fba1d9194f44fac62f",
+    "url": "/peanuts/static/js/main.e3fba1d9.chunk.js"
+  },
+  {
+    "revision": "788a3f637229d1d4e776",
+    "url": "/peanuts/static/js/1.788a3f63.chunk.js"
+  },
+  {
+    "revision": "e3fba1d9194f44fac62f",
+    "url": "/peanuts/static/css/main.cec9458e.chunk.css"
+  },
+  {
+    "revision": "788a3f637229d1d4e776",
+    "url": "/peanuts/static/css/1.82dccb8e.chunk.css"
+  },
+  {
+    "revision": "a644b6fe2efa6063cc892ee0c644b523",
+    "url": "/peanuts/index.html"
+  }
+];
